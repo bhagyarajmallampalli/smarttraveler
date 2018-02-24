c3dd454fb983c0612442496089a09e788fec0458
bhagyarajmallampalli <bhagyaraj.mallampalli@gmail.com>
2018-02-25 00:04:45 +0100

    removed unnecessary html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
